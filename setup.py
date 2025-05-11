0503e1f02ffeb85698af3738ae37e57d6e042e77
Sudo-Ivan <Sudo-Ivan@users.noreply.github.com>
2025-05-11 01:27:12 -0500

    remove redundant parameter

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("README.md", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r", encoding="utf-8") as f:
+with open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
 setuptools.setup(
